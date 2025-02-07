d4a3d6fd520aa56151c29aff4d7e5f1096efbd4e
Supriya <supriyagole12@gmail.com>
2025-02-08 00:17:12 +0100

    feat: Introduce footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <p className={styles.message}>
+            Released under the MIT License.
+        </p>
+        <p className={styles.message}>
+            Copyright © 2025-present X Contributors
+        </p>
+      </div>
+    </footer>
+  )
+};
+
+export default Footer;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import styles from "./Layout.module.css";
 
@@ -6,6 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode[] }) => {
     <div className={styles.layout}>
       <Header />
       <main>{children}</main>
+      <Footer />
     </div>
   );
 };
